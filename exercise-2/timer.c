35f04231dbc9820cce1c2c79cad06e977f89bfc8
Anders <anderssp1994@gmail.com>
2018-09-19 19:31:45 +0200

    Implement timer setup

---
+++ exercise-2/timer.c
@@ -0,0 +1,36 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "efm32gg.h"
+
+/*
+ * function to setup the timer 
+ */
+void setupTimer(uint16_t period)
+{
+	/*
+	 * TODO enable and set up the timer
+	 * 
+	 * 1. Enable clock to timer by setting bit 6 in CMU_HFPERCLKEN0 2.
+	 * Write the period to register TIMER1_TOP 3. Enable timer interrupt
+	 * generation by writing 1 to TIMER1_IEN 4. Start the timer by writing 
+	 * 1 to TIMER1_CMD
+	 * 
+	 * This will cause a timer interrupt to be generated every (period)
+	 * cycles. Remember to configure the NVIC as well, otherwise the
+	 * interrupt handler will not be invoked. 
+	 */
+
+    // Enable clock timer
+    *CMU_HFPERCLKEN0 |= (1<<6);
+
+    // Set period
+    *TIMER1_TOP = 7*100*1000;
+
+    // Enabe timer interrupt generation
+    *TIMER1_IEN |= 1;
+
+    // Start timer
+    *TIMER1_CMD |= 1;
+}
+
