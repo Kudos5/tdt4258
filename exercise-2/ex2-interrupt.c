943ea0e3511192283d3e49be82a886f5c4241287
Anders <anderssp1994@gmail.com>
2018-10-30 13:08:34 +0100

    Implement sleeping in interrupt version

--- exercise-2/ex2-interrupt.c
+++ exercise-2/ex2-interrupt.c
@@ -55,7 +55,9 @@ int main(void)
 	 * TODO for higher energy efficiency, sleep while waiting for
 	 * interrupts instead of infinite loop for busy-waiting 
 	 */
-	while (1) ;
+	while (1) {
+        __asm__ ("wfi;");
+    }
 
 	return 0;
 }
