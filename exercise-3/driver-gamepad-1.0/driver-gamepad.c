c9b063fb0149b4278fbf9b935c1ebc554953c2e4
Anders <anderssp1994@gmail.com>
2018-11-20 18:25:40 +0100

    Clean up driver code

--- exercise-3/driver-gamepad-1.0/driver-gamepad.c
+++ exercise-3/driver-gamepad-1.0/driver-gamepad.c
@@ -14,22 +14,48 @@
 #include "driver-gamepad.h"
 #include "gpio.h"
 
+// Module parameters
 #define DRIVER_NAME "gamepad"
 
+// Function forward declarations
+static int gp_probe(struct platform_device * p_dev_ptr);
+static int gp_remove(struct platform_device * dev);
+static int gp_open(struct inode * inode, struct file * file_pointer);
+static int gp_release(struct inode * inode, struct file * file_pointer);
+static long gp_ioctl(struct file * file_pointer, unsigned int cmd, unsigned long arg);
+static int gp_fasync(int fd, struct file *filp, int on);
+
 // Module variables
-int long unsigned gp_owner_pid = -1;
 static struct class * gp_cl;
-static int gp_major;
-static int gp_minor;
 struct cdev * gp_cdev_ptr;
 dev_t gp_dev_number;
 static struct fasync_struct * gp_async_queue;
 static int unsigned gp_button_state;
+static struct file_operations gp_fops = {
+    .owner = THIS_MODULE,
+    .open = gp_open,
+    .release = gp_release,
+    .unlocked_ioctl = gp_ioctl,
+    .fasync = gp_fasync,
+};
+static const struct of_device_id gp_of_match[] = {
+    { .compatible = "tdt4258", },
+    { },
+};
+static struct platform_driver gp_driver = {
+    .probe = gp_probe,
+    .remove = gp_remove,
+    .driver = {
+        .name = "gp",
+        .owner = THIS_MODULE,
+        .of_match_table = gp_of_match,
+    },
+};
 
-static void setupGPIO(volatile void * gpio_base)
+// Function definitions
+
+static void setup_gpio(volatile void * gpio_base)
 {
-    // TODO: Implement without hardcoding adresses.
-    // Is it possible to get all adresses, or do we have to hardcode offsets?
     int long unsigned current_value;
     int long unsigned new_value;
 
@@ -61,26 +87,8 @@ static void setupGPIO(volatile void * gpio_base)
     iowrite32(new_value, gpio_base + GPIO_IFC);
 }
 
-/*
-static void PrintPDev(struct platform_device * dev) {
-    int i;
-    struct resource * res;
-    printk("gp_probe called\n");
-    printk("gamepad driver registered to device with following information:\n");
-    printk("name: %s\n", dev->name);
-    printk("id: %d\n", dev->id);
-    printk("num resources: %d\n", dev->num_resources);
-    printk("Resources:\n");
-    for ( i = 0; i < dev->num_resources; ++i ) {
-        res = platform_get_resource(dev, IORESOURCE_MEM, i);
-        printk("Name: %s\n", res->name);
-        printk("Start: %#08X\n", res->start);
-        printk("End: %#08X\n", res->end);
-    }
-}
-*/
-
-static irqreturn_t isr_store_button_state(int irq, void * dev) {
+static irqreturn_t isr_store_button_state(int irq, void * dev)
+{
     int long unsigned current_value;
     int long unsigned new_value;
 
@@ -100,15 +108,18 @@ static irqreturn_t isr_store_button_state(int irq, void * dev) {
     return IRQ_HANDLED;
 }
 
-static int gp_open(struct inode * inode, struct file * file_pointer) {
+static int gp_open(struct inode * inode, struct file * file_pointer)
+{
     return 0;
 }
 
-static int gp_release(struct inode * inode, struct file * file_pointer) {
+static int gp_release(struct inode * inode, struct file * file_pointer)
+{
     return 0;
 }
 
-static long gp_ioctl(struct file * file_pointer, unsigned int cmd, unsigned long arg) {
+static long gp_ioctl(struct file * file_pointer, unsigned int cmd, unsigned long arg)
+{
     switch (cmd) {
         case GP_IOCTL_GET_BUTTON_STATE:
             return gp_button_state;
@@ -117,7 +128,8 @@ static long gp_ioctl(struct file * file_pointer, unsigned int cmd, unsigned long
     return 0;
 }
 
-static int gp_fasync(int fd, struct file *filp, int on) {
+static int gp_fasync(int fd, struct file *filp, int on)
+{
     int temp;
     temp = fasync_helper(fd, filp, on, &gp_async_queue);
     if (fd != -1)
@@ -125,25 +137,16 @@ static int gp_fasync(int fd, struct file *filp, int on) {
     return temp;
 }
 
-static struct file_operations gp_fops = {
-    .owner = THIS_MODULE,
-    .open = gp_open,
-    .release = gp_release,
-    .unlocked_ioctl = gp_ioctl,
-    .fasync = gp_fasync,
-};
-
-static int RegisterChrDev(void) {
+static int register_chr_dev(void)
+{
     int ret;
     if ( (ret = alloc_chrdev_region(&gp_dev_number, 0, 1, DRIVER_NAME)) < 0 ) {
-        printk("ERROR %d: Failed alloc_chr_region %s\n", ret, DRIVER_NAME);
+        printk(KERN_ERR "ERROR %d: Failed alloc_chr_region %s\n", ret, DRIVER_NAME);
     }
-    gp_major = MAJOR(gp_dev_number);
-    gp_minor = MINOR(gp_dev_number);
     gp_cdev_ptr = cdev_alloc();
     cdev_init(gp_cdev_ptr, &gp_fops);
     if ( (ret = cdev_add(gp_cdev_ptr, gp_dev_number, 1)) < 0 ) {
-        printk("ERROR %d: Failed to add cdev %s\n", ret, DRIVER_NAME);
+        printk(KERN_ERR "ERROR %d: Failed to add cdev %s\n", ret, DRIVER_NAME);
     }
     gp_cl = class_create(THIS_MODULE, DRIVER_NAME);
     device_create(gp_cl, NULL, gp_dev_number, NULL, DRIVER_NAME);
@@ -151,18 +154,21 @@ static int RegisterChrDev(void) {
     return 0;
 }
 
-static void UnregisterChrDev(void) {
+static void unregister_chr_dev(void)
+{
     unregister_chrdev_region(gp_dev_number, 1);
     cdev_del(gp_cdev_ptr);
     class_destroy(gp_cl);
 }
 
-static int gp_probe(struct platform_device * p_dev_ptr) {
+static int gp_probe(struct platform_device * p_dev_ptr)
+{
     int gpio_even_irq;
     int gpio_odd_irq;
     struct resource * res;
     int gpio_base_address;
-    RegisterChrDev();
+    int ret;
+    register_chr_dev();
 
     // Get info about GPIO
     res = platform_get_resource(p_dev_ptr, IORESOURCE_MEM, 0);
@@ -170,23 +176,29 @@ static int gp_probe(struct platform_device * p_dev_ptr) {
     // Get GPIO IRQ number
     gpio_even_irq = platform_get_irq(p_dev_ptr, 0);
     gpio_odd_irq = platform_get_irq(p_dev_ptr, 1);
-    setupGPIO((volatile void *)gpio_base_address);
-    // Register an interrupt
-    if (request_irq(gpio_even_irq, isr_store_button_state, IRQF_SHARED, "gpio_even", p_dev_ptr)) {
-        printk(KERN_ERR "rtc: cannot register IRQ %d\n", gpio_even_irq);
+    setup_gpio((volatile void *)gpio_base_address);
+    // Register even interrupt
+    ret = request_irq(gpio_even_irq, isr_store_button_state, IRQF_SHARED,
+            "gpio_even", p_dev_ptr);
+    if (ret) {
+        printk(KERN_ERR "ERROR %d: cannot register IRQ %d\n", ret, gpio_even_irq);
         return -EIO;
     }
-    if (request_irq(gpio_odd_irq, isr_store_button_state, IRQF_SHARED, "gpio_odd", p_dev_ptr)) {
-        printk(KERN_ERR "rtc: cannot register IRQ %d\n", gpio_odd_irq);
+    // Register odd interrupt
+    ret = request_irq(gpio_odd_irq, isr_store_button_state, IRQF_SHARED,
+            "gpio_odd", p_dev_ptr);
+    if (ret) {
+        printk(KERN_ERR "ERROR %d: cannot register IRQ %d\n", ret, gpio_odd_irq);
         return -EIO;
     }
     return 0;
 }
 
-static int gp_remove(struct platform_device * dev) {
+static int gp_remove(struct platform_device * dev)
+{
     int gpio_even_irq;
     int gpio_odd_irq;
-    UnregisterChrDev();
+    unregister_chr_dev();
     gpio_even_irq = platform_get_irq(dev, 0);
     gpio_odd_irq = platform_get_irq(dev, 1);
     free_irq(gpio_even_irq, dev);
@@ -194,35 +206,22 @@ static int gp_remove(struct platform_device * dev) {
     return 0;
 }
 
-static const struct of_device_id gp_of_match[] = {
-    { .compatible = "tdt4258", },
-    { },
-};
-MODULE_DEVICE_TABLE(of, gp_of_match);
-
-static struct platform_driver gp_driver = {
-    .probe = gp_probe,
-    .remove = gp_remove,
-    .driver = {
-        .name = "gp",
-        .owner = THIS_MODULE,
-        .of_match_table = gp_of_match,
-    },
-};
-
-static int __init gp_init(void) {
+static int __init gp_init(void)
+{
     int ret;
     // Register as platform driver
     if ( (ret = platform_driver_register(&gp_driver)) != 0 ) {
-        printk("Failed to register platform device: %d\n", ret);
+        printk(KERN_ERR "ERROR %d: Failed to register platform device\n", ret);
         return ret;
     }
 	return 0;
 }
 
-static void __exit gp_cleanup(void) {
+static void __exit gp_cleanup(void)
+{
 }
 
+MODULE_DEVICE_TABLE(of, gp_of_match);
 module_init(gp_init);
 module_exit(gp_cleanup);
 
